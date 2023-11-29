129039a274b50f4a0d8f049a045e83cbac56673c
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-11-29 06:56:21 +0300

    Add linters:rubocop,stylelint

--- db/migrate/20231129030943_create_posts.rb
+++ db/migrate/20231129030943_create_posts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
@@ -6,7 +8,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
       t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
-      
+
       t.timestamps
     end
   end
