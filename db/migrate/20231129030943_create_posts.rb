5661a338c51113090727b3caa929d629aa684cad
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-11-29 07:51:43 +0300

    use author instead of :user in create_posts model

--- db/migrate/20231129030943_create_posts.rb
+++ db/migrate/20231129030943_create_posts.rb
@@ -3,7 +3,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :user, foreign_key: { to_table: :users }, index: true, null: false
+      t.references :author, foreign_key: { to_table: :users }, index: true, null: false
       t.string :title
       t.text :text
       t.integer :comments_counter
