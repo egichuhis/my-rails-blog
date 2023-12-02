08ebac97393c56c77e327eae371413f807233b71
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-02 11:11:43 +0300

    Add comment_test & like_test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,22 @@
+# test/models/comment_test.rb
 require 'test_helper'
+require 'factory_bot'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include FactoryBot::Syntax::Methods
+
+  def setup
+    @user = create(:user)
+    @post = create(:post, author: @user)
+  end
+
+  test 'comment should be valid' do
+    comment = build(:comment, user: @user, post: @post)
+    assert comment.valid?
+  end
+
+  test 'update_post_comments_counter should update the comments counter for the post' do
+    create(:comment, user: @user, post: @post)
+    assert_equal 1, @post.reload.comments_counter
+  end
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,22 @@
+# test/models/like_test.rb
 require 'test_helper'
+require 'factory_bot'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include FactoryBot::Syntax::Methods
+
+  def setup
+    @user = create(:user)
+    @post = create(:post, author: @user)
+  end
+
+  test 'like should be valid' do
+    like = build(:like, user: @user, post: @post)
+    assert like.valid?
+  end
+
+  test 'update_post_likes_counter should update the likes counter for the post' do
+    create(:like, user: @user, post: @post)
+    assert_equal 1, @post.reload.likes_counter
+  end
 end
