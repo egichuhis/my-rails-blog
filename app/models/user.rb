bb9653c16695b9d0b61240fa1651cde125ed105c
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-09 16:04:30 +0300

    Update database with data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   private
 
