9131e85ec747d2e387e9700ec05053dbca3c9f00
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-10 14:45:52 +0300

    Update seeds.rb with seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # user.rb
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :name, presence: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,41 @@
-# frozen_string_literal: true
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# seeds.rb
+
+# Clear existing data
+User.destroy_all
+
+# Create users
+first_user = User.create(
+  name: 'Tom',
+  photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+  bio: 'Plumber from Mexico.'
+)
+
+second_user = User.create(
+  name: 'Lilly',
+  photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+  bio: 'Teacher from Poland.'
+)
+
+third_user = User.create(
+  name: 'Conny',
+  photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+  bio: 'Engineer from Holand.'
+)
+
+first_post = Post.create(author: first_user, title: 'Hello 1', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello 2', text: 'This is my second post')
+third_post = Post.create(author: second_user, title: 'Hello 1', text: 'This is my first post')
+fourth_post = Post.create(author: second_user, title: 'Hello 2', text: 'This is my second post')
+fifth_post = Post.create(author: third_user, title: 'Hello 1', text: 'This is my first post')
+
+Comment.create(post: first_post, user: first_user, text: 'Hi Tom!' )
+Comment.create(post: second_post, user: first_user, text: 'Hi Tom 2!' )
+Comment.create(post: third_post, user: second_user, text: 'Hi Rom!' )
+Comment.create(post: fourth_post, user: second_user, text: 'Hi Fom 2!' )
+Comment.create(post: fifth_post, user: third_user, text: 'Hi Dom!' )
+
+
+Like.create(post: first_post, user: first_user)
+Like.create(post: second_post, user: first_user)
+Like.create(post: fifth_post, user: third_user)
+
