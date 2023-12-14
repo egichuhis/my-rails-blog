90b572aee00a39af0c4dbd06f40531fb4f233ff0
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-14 19:07:26 +0300

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,1 @@
 # users_controller.rb
-
-class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
-
-  def index
-    @users = User.all
-    @post = Post.new
-    @user = current_user
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.recent_posts
-  end
-end
