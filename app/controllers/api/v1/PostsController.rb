548536108094549ce3639f8b6a9c28c36eb84c0b
Md Mahadi Hasan Rifat <123451855+Rifat8080@users.noreply.github.com>
2023-12-15 12:00:41 +0600

    Update PostsController.rb

--- app/controllers/api/v1/PostsController.rb
+++ app/controllers/api/v1/PostsController.rb
@@ -1,1 +1,12 @@
-# PostsController
+
+class Api::V1::PostsController < ApplicationController
+  def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      posts = user.posts
+    else
+      posts = Post.all
+    end
+    render json: posts
+  end
+end
