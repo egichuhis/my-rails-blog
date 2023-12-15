141a85caacb3e8c6845db9682b693bb40edeb4fb
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-15 08:39:11 +0300

    Delete post and comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create] do
+    resources :posts, only: [:index, :show, :create, :destroy] do
       resources :likes, only: [:create, :index]
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :destroy]
     end
   end
 end
