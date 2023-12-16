cb77e4bbf30810441b575babb0474fb2567570bd
Md Mahadi Hasan Rifat <123451855+Rifat8080@users.noreply.github.com>
2023-12-16 10:59:37 +0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # controllers/application_controller.rb
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
